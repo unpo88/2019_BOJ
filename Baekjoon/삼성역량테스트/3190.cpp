425c4ca35b344a5cc23472bfe4530e661a75e93d
최원섭 <whatsup@khu.ac.kr>
2019-03-03 22:52:37 +0900

    Baekjoon 3190 뱀

--- Baekjoon/삼성역량테스트/3190.cpp
+++ Baekjoon/삼성역량테스트/3190.cpp
@@ -9,89 +9,103 @@
 // 8. 뱀은 몸 길이를 늘려서 머리를 다음칸에 위치시킨다.
 // 9. 이동한 칸에 사과가 있으면 사과가 없어지고, 꼬리는 움직이지 않는다.
 // 10. 이동한 칸에 사과가 없으면 몸길이를 줄여서 꼬리가 위치한 칸을 비운다. 즉, 몸 길이는 변하지 않는다.
-// 11. 사과의 위치와 뱀의 이동경로가 주어질 대 이 게임은 몇 초에 끝나는지 구하시오.
+// 11. 사과의 위치와 뱀의 이동경로가 주어질 대 이 게임은 몇 초에 끝나는지 구하시오. 
+
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-#define MAX_SIZE 100
+// 보드 크기, 사과 개수, 방향 변환 횟수
+int n, k, m;
 
-// 오른쪽, 아래, 왼쪽, 위
+// 보드 크기
+int map[101][101];
+
+// 머리, 꼬리 인덱스
+int head_y, head_x, tail_index;
+
+// 뱀의 좌표
+int snake_y[10101], snake_x[10101];
+
+// 명령어
+char cmd[10001];
+
+// 오른쪽 아래 왼쪽 위
 const int dy[] = { 0, 1, 0, -1 };
 const int dx[] = { 1, 0, -1, 0 };
 
-// 사과 위치
-bool apple[MAX_SIZE][MAX_SIZE] = { false, };
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
 
-// 지도
-int board[MAX_SIZE][MAX_SIZE];
+	cin >> n >> k;
 
-// 크기
-int N;
+	int y, x;
+	for (int i = 0; i < k; ++i) {
+		cin >> y >> x;
+		map[y][x] = 1;
+	}
 
-// 사과 개수
-int K;
+	int time;
+	char c;
 
-// 명령어 개수
-int L;
+	cin >> m;
+	for (int i = 0; i < m; ++i) {
+		cin >> time >> c;
+		cmd[time] = c;
+	}
 
-// 명령어 형태
-// 초, (왼, 오)
-int M;
-char ch;
+	int dir = 0;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL),  cout.tie(NULL);
+	// 처음 시간은 0
+	time = 0;
 
-    cin >> N;
-    cin >> K;
-    while(K--){
-        int y, x;
-        cin >> y >> x;
-        apple[y - 1][x - 1] = true;
-    }
+	// 최초 위치는 맨 왼쪽 맨 위, 인덱스는 시간과 같음
+	head_y = 1, head_x = 1, tail_index = time;
 
-    memset(board, -1, sizeof(board));
+	// 좌표를 저장
+	snake_y[time] = head_y, snake_x[time] = head_x;
+	
+	// 뱀이 있을 때는 -1
+	map[head_y][head_x] = -1;
 
-    int x = 0;
-    int y = 0;
-    int dir = 0;
-    int len = 1;
-    board[y][x] = 0;
-    cin >> L;
-    int now = 0;
-    
+	// 시간은 계속 지나가기 때문에 무한루프
+	while (1) {
+		++time;
 
-    for(int k = 0; k <= L; k++){
-        M = N * N + 1;
-        ch = 'L';
-        if(k < L){
-            cin >> M >> ch;
-        }
-        while(now < M){
-            now += 1;
-            x += dx[dir];
-            y += dy[dir];
-            if(x < 0 || x >= N || y < 0 || y >= N){
-                cout << now << '\n';
-                return 0;
-            }
-            if(apple[y][x]){
-                apple[y][x] = false;
-                len += 1;
-            }
-            if(board[y][x] != -1 && now - board[y][x] <= len){
-                cout << now << '\n';
-                return 0;
-            }
-            board[y][x] = now;
-        }
-        if(ch == 'L'){
-            dir = (dir + 3) % 4;
-        }else{
-            dir = (dir + 1) % 4;
-        }
-    }
-    return 0;
+		// 뱀이 이동경로대로 계속해서 움직인다.
+		head_y += dy[dir];
+		head_x += dx[dir];
+
+		// 다음 조건이면 무조건 끝남 범위를 벗어나거나 자기 자신을 만나거나
+		if (head_y < 1 || head_y > n || head_x < 1 || head_x > n || map[head_y][head_x] == -1) {
+			break;
+		}
+
+		// 시간마다 좌표를 저장
+		snake_y[time] = head_y;
+		snake_x[time] = head_x;
+
+		// 만약에 사과가 없다면, 뱀의 꼬리를 한칸 줄여준다.
+		if (map[head_y][head_x] == 0) {
+			int tail_y = snake_y[tail_index];
+			int tail_x = snake_x[tail_index];
+			map[tail_y][tail_x] = 0;	// 꼬리를 줄여줬기 때문에 0으로 바꾼다. -1을 0으로 바꾸어줌
+			++tail_index;
+		}
+		// 뱀의 머리가 위치한 곳에 -1
+		map[head_y][head_x] = -1;
+
+		// 오른쪽 그리고 왼쪽으로 회전할 때
+		if (cmd[time] == 'D') {
+			dir = (dir + 1) % 4;
+		}
+
+		if (cmd[time] == 'L') {
+			dir = (dir + 3) % 4;
+		}
+	}
+
+	cout << time << "\n";
+
+	return 0;
 }
