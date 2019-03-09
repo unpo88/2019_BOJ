3518bfd40d287067d4cbe6d5c2fedb94f4e5c4dd
Sub <whatsup@khu.ac.kr>
2019-03-09 23:07:47 +0900

    [Update] Baekjoon 뱀

--- Baekjoon/삼성역량테스트/3190.cpp
+++ Baekjoon/삼성역량테스트/3190.cpp
@@ -1,111 +1,93 @@
-// [시뮬레이션 문제]
-// 1. 사과를 먹으면 뱀의 길이가 늘어난다.
-// 2. 벽 또는 자기 자신의 몸과 부딪치면 게임은 끝난다.
-// 3. 몇몇 칸에는 사과가 있을 수 있다.
-// 4. 보드의 상하좌우 끝에 벽이 있다.
-// 5. 뱀은 맨위 맨좌측에서 시작한다. 뱀의 길이는 1이다.
-// 6. 맨위 맨좌측에는 사과가 없다.
-// 7. 뱀의 첫 시작은 오른쪽을 향한다.
-// 8. 뱀은 몸 길이를 늘려서 머리를 다음칸에 위치시킨다.
-// 9. 이동한 칸에 사과가 있으면 사과가 없어지고, 꼬리는 움직이지 않는다.
-// 10. 이동한 칸에 사과가 없으면 몸길이를 줄여서 꼬리가 위치한 칸을 비운다. 즉, 몸 길이는 변하지 않는다.
-// 11. 사과의 위치와 뱀의 이동경로가 주어질 대 이 게임은 몇 초에 끝나는지 구하시오. 
+// [시뮬레이션]
+// 게임은 NxN 정사각 보드위에서 진행되고, 몇몇 칸에는 사과가 놓여져 있다. 보드의 상하좌우 끝에 벽이 있다.
+
 
 #include <iostream>
 using namespace std;
 
-// 보드 크기, 사과 개수, 방향 변환 횟수
-int n, k, m;
+// 게임은 NxN 정사각 보드위에서 진행되고, 몇몇 칸에는 사과가 놓여져 있다. 보드의 상하좌우 끝에 벽이 있다.
+int N, K, L, X;
+char C;
+int board[101][101];
 
-// 보드 크기
-int map[101][101];
-
-// 머리, 꼬리 인덱스
 int head_y, head_x, tail_index;
-
-// 뱀의 좌표
 int snake_y[10101], snake_x[10101];
-
-// 명령어
 char cmd[10001];
 
+// 90도 방향을 회전시킨다
 // 오른쪽 아래 왼쪽 위
 const int dy[] = { 0, 1, 0, -1 };
 const int dx[] = { 1, 0, -1, 0 };
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
+int main(){
+    //   3.첫째 줄에 보드의 크기 N이 주어진다. (2 ≤ N ≤ 100)
+    //     다음 줄에 사과의 개수 K가 주어진다. (0 ≤ K ≤ 100)
+    cin >> N >> K;
 
-	cin >> n >> k;
+    //   4. 다음 K개의 줄에는 사과의 위치가 주어지는데, 첫 번째 정수는 행, 두 번째 정수는 열 위치를 의미한다. 
+    //      사과의 위치는 모두 다르며, 맨 위 맨 좌측 (1행 1열) 에는 사과가 없다.
+    while(K--){
+        int y, x;
+        cin >> y >> x;
+        board[y][x] = 1;
+    }
 
-	int y, x;
-	for (int i = 0; i < k; ++i) {
-		cin >> y >> x;
-		map[y][x] = 1;
-	}
+    //   5. 다음 줄에는 뱀의 방향 변환 횟수 L 이 주어진다. (1 ≤ L ≤ 100)
+    cin >> L;
+    //   6. 다음 L개의 줄에는 뱀의 방향 변환 정보가 주어지는데,  정수 X와 문자 C로 이루어져 있으며. 
+    //      X초가 끝난 뒤에 왼쪽(C가 'L') 또는 오른쪽(C가 'D')로 90도 방향을 회전시킨다는 뜻이다.
+    //      X는 10,000 이하의 양의 정수이며, 방향 전환 정보는 X가 증가하는 순으로 주어진다.
+    while(L--){
+        cin >> X >> C;
+        cmd[X] = C;
+    }
 
-	int time;
-	char c;
+    int time = 0;
 
-	cin >> m;
-	for (int i = 0; i < m; ++i) {
-		cin >> time >> c;
-		cmd[time] = c;
-	}
+    //   1. 게임이 시작할때 뱀은 맨위 맨좌측에 위치하고 뱀의 길이는 1 이다. 뱀은 처음에 오른쪽을 향한다.
+    head_y = 1, head_x = 1, tail_index = time;
+    snake_y[time] = head_y, snake_x[time] = head_x;
+    int dir = 0;
 
-	int dir = 0;
+    // 뱀이 있는 곳을 -1이라고 한다.
+    board[snake_y[time]][snake_x[time]] = -1;
 
-	// 처음 시간은 0
-	time = 0;
+    //   2. 뱀은 매 초마다 이동을 하는데 다음과 같은 규칙을 따른다.
+    while(1){
+        ++time;
+        //      2-1. 먼저 뱀은 몸길이를 늘려 머리를 다음칸에 위치시킨다.
+        // 뱀이 이동경로대로 계속해서 움직인다.
+        head_y += dy[dir],  head_x += dx[dir];
 
-	// 최초 위치는 맨 왼쪽 맨 위, 인덱스는 시간과 같음
-	head_y = 1, head_x = 1, tail_index = time;
-
-	// 좌표를 저장
-	snake_y[time] = head_y, snake_x[time] = head_x;
-	
-	// 뱀이 있을 때는 -1
-	map[head_y][head_x] = -1;
-
-	// 시간은 계속 지나가기 때문에 무한루프
-	while (1) {
-		++time;
-
-		// 뱀이 이동경로대로 계속해서 움직인다.
-		head_y += dy[dir];
-		head_x += dx[dir];
-
-		// 다음 조건이면 무조건 끝남 범위를 벗어나거나 자기 자신을 만나거나
-		if (head_y < 1 || head_y > n || head_x < 1 || head_x > n || map[head_y][head_x] == -1) {
-			break;
-		}
+        // 사과를 먹으면 뱀 길이가 늘어난다. 뱀이 이리저리 기어다니다가 벽 또는 자기자신의 몸과 부딪히면 게임이 끝난다.
+        if(head_y < 1 || head_x < 1 || head_y > N || head_x > N || board[head_y][head_x] == -1){
+            break;
+        }
 
 		// 시간마다 좌표를 저장
-		snake_y[time] = head_y;
-		snake_x[time] = head_x;
+        snake_y[time] = head_y, snake_x[time] = head_x;
+        //      2-2. 만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지 않는다.
+        //      2-3. 만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다. 즉, 몸길이는 변하지 않는다.
+        if(board[head_y][head_x] == 0){
+            int tail_y = snake_y[tail_index];
+            int tail_x = snake_x[tail_index];
+            board[tail_y][tail_x] = 0;
+            ++tail_index;
+        }
+        board[head_y][head_x] = -1;
 
-		// 만약에 사과가 없다면, 뱀의 꼬리를 한칸 줄여준다.
-		if (map[head_y][head_x] == 0) {
-			int tail_y = snake_y[tail_index];
-			int tail_x = snake_x[tail_index];
-			map[tail_y][tail_x] = 0;	// 꼬리를 줄여줬기 때문에 0으로 바꾼다. -1을 0으로 바꾸어줌
-			++tail_index;
-		}
-		// 뱀의 머리가 위치한 곳에 -1
-		map[head_y][head_x] = -1;
-
-		// 오른쪽 그리고 왼쪽으로 회전할 때
-		if (cmd[time] == 'D') {
+        if (cmd[time] == 'D') {
 			dir = (dir + 1) % 4;
 		}
 
 		if (cmd[time] == 'L') {
 			dir = (dir + 3) % 4;
 		}
-	}
+    }
 
-	cout << time << "\n";
+    //      2-4. 사과의 위치와 뱀의 이동경로가 주어질 때 이 게임이 몇 초에 끝나는지 계산하라.
+    //   7. 첫째 줄에 게임이 몇 초에 끝나는지 출력한다.
+    cout << time << "\n";
 
-	return 0;
+    return 0;
 }
