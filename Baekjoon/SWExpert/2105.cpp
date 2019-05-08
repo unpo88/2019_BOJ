d852c5eda294c377502c2026bee153b4b6dc8050
Sub <whatsup@khu.ac.kr>
2019-05-09 01:18:52 +0900

    [Update] SWExpert 디저트카페

--- Baekjoon/SWExpert/2105.cpp
+++ Baekjoon/SWExpert/2105.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 using namespace std;
 
+int N;
+
+int map[20][20];
+
+void init(){
+    for(int y = 0; y < 20; ++y){
+        for(int x = 0; x < 20; ++x){
+            map[y][x] = 0;
+        }
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL),  cout.tie(NULL);
-
+    
     int T;
+
     cin >> T;
 
     for(int tc = 1; tc <= T; ++tc){
-        int N;
-        cin >> N;
-        int map[20][20] = { 0, };
-
         int answer = -1;
+        init();
+
+        cin >> N;
+
         for(int y = 0; y < N; ++y){
             for(int x = 0; x < N; ++x){
                 cin >> map[y][x];
@@ -24,22 +37,24 @@ int main(){
             for(int x = 0; x < N; ++x){
                 for(int a = 1; a <= N; ++a){
                     for(int b = 1; b <= N; ++b){
-                        if(y + a < N 
-                        && x + a < N
+                        if(y + b < N
+                        && x - b >= 0
                         && y + a + b < N
                         && x + a - b >= 0
-                        && y + b < N
-                        && x - b >= 0
-                        && answer < 2 * (a + b)){
-                            int visited[101] = { false, };
-                            bool isAble = true;
+                        && y + a < N
+                        && x + a < N
+                        && answer < (a + b) * 2){
                             int cur_y = y;
                             int cur_x = x;
 
-                            for(int n = 0; n < a; ++n){
+                            bool check[101] = { false, };
+                            bool isAble = true;
+
+
+                            for(int i = 1; i <= a; ++i){
                                 cur_y++;    cur_x++;
-                                if(!visited[map[cur_y][cur_x]]){
-                                    visited[map[cur_y][cur_x]] = true;
+                                if(!check[map[cur_y][cur_x]]){
+                                    check[map[cur_y][cur_x]] = true;
                                 }else{
                                     isAble = false;
                                     break;
@@ -47,10 +62,10 @@ int main(){
                             }
                             if(!isAble) continue;
 
-                            for(int n = 0; n < b; ++n){
+                            for(int i = 1; i <= b; ++i){
                                 cur_y++;    cur_x--;
-                                if(!visited[map[cur_y][cur_x]]){
-                                    visited[map[cur_y][cur_x]] = true;
+                                if(!check[map[cur_y][cur_x]]){
+                                    check[map[cur_y][cur_x]] = true;
                                 }else{
                                     isAble = false;
                                     break;
@@ -58,10 +73,10 @@ int main(){
                             }
                             if(!isAble) continue;
 
-                            for(int n = 0; n < a; ++n){
+                            for(int i = 1; i <= a; ++i){
                                 cur_y--;    cur_x--;
-                                if(!visited[map[cur_y][cur_x]]){
-                                    visited[map[cur_y][cur_x]] = true;
+                                if(!check[map[cur_y][cur_x]]){
+                                    check[map[cur_y][cur_x]] = true;
                                 }else{
                                     isAble = false;
                                     break;
@@ -69,10 +84,10 @@ int main(){
                             }
                             if(!isAble) continue;
 
-                            for(int n = 0; n < b; ++n){
-                                cur_y--;    cur_x++;
-                                if(!visited[map[cur_y][cur_x]]){
-                                    visited[map[cur_y][cur_x]] = true;
+                            for(int i = 1; i <= b; ++i){
+                                cur_y--; cur_x++;
+                                if(!check[map[cur_y][cur_x]]){
+                                    check[map[cur_y][cur_x]] = true;
                                 }else{
                                     isAble = false;
                                     break;
@@ -80,7 +95,7 @@ int main(){
                             }
                             if(!isAble) continue;
 
-                            answer = 2 * (a + b);
+                            answer = (a + b) * 2;
                         }
                     }
                 }
