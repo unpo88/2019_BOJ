2bff0178dd9a0de496e53c30ad3e2d95205706ed
Sub <whatsup@khu.ac.kr>
2019-05-09 02:20:07 +0900

    [Update] SWExpert 1949 등산로 조정

--- Baekjoon/SWExpert/1949.cpp
+++ Baekjoon/SWExpert/1949.cpp
@@ -3,53 +3,43 @@
 #include <utility>
 using namespace std;
 
-int N, K;
-
 int map[8][8];
 bool visited[8][8];
 
-const int dy[] = { 0, 0, 1, -1 };
-const int dx[] = { 1, -1, 0 ,0 };
+int N, K;
 
-int height;
+const int dy[] = { 0, 0, 1, -1 };
+const int dx[] = { 1, -1, 0, 0 };
+
 int answer;
 
-void check_height(){
-    for(int y = 0; y < N; ++y){
-        for(int x = 0; x < N; ++x){
-            if(height < map[y][x])  height = map[y][x];
+void init(){
+    for(int y = 0; y < 8; ++y){
+        for(int x = 0; x < 8; ++x){
+            map[y][x] = 0;
         }
     }
+    answer = 0;
 }
 
-void input(){
-    cin >> N >> K;
-    for(int y = 0; y < N; ++y){
-        for(int x = 0; x < N; ++x){
-            cin >> map[y][x];
-        }
-    }
-}
-
-void DFS(int y, int x, int len, int status){
+void DFS(int y, int x, int cnt, int status){
     visited[y][x] = true;
-    
     for(int dir = 0; dir < 4; ++dir){
         int ny = y + dy[dir];
         int nx = x + dx[dir];
         if(ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx])  continue;
         if(map[y][x] > map[ny][nx]){
-            DFS(ny, nx, len + 1, status);
+            DFS(ny, nx, cnt + 1, status);
         }else if(!status && map[y][x] > map[ny][nx] - K){
             int temp = map[ny][nx];
             map[ny][nx] = map[y][x] - 1;
-            DFS(ny, nx, len + 1, 1);
+            DFS(ny, nx, cnt + 1, 1);
             map[ny][nx] = temp;
         }
     }
 
     visited[y][x] = false;
-    answer = max(answer, len);
+    answer = max(answer, cnt);
 }
 
 int main(){
@@ -60,21 +50,33 @@ int main(){
     cin >> T;
 
     for(int tc = 1; tc <= T; ++tc){
-        answer = 0;
-        height = 0;
+        init();
 
-        input();
-        check_height();
+        cin >> N >> K;
 
         vector<pair<int, int> > v;
+        
+        int height = 0;
+         
         for(int y = 0; y < N; ++y){
             for(int x = 0; x < N; ++x){
-                if(height == map[y][x]) v.push_back(make_pair(y,x));
+                cin >> map[y][x];
+                if(height < map[y][x]){
+                    height = map[y][x];
+                }
             }
         }
 
-        for(int i = 0; i < v.size(); ++i){
-            DFS(v[i].first, v[i].second, 1, 0);
+        for(int y = 0; y < N; ++y){
+            for(int x = 0; x < N; ++x){
+                if(map[y][x] == height){
+                    v.push_back(make_pair(y, x));
+                }
+            }
+        }
+
+        for(int index = 0; index < v.size(); ++index){
+            DFS(v[index].first, v[index].second, 1, 0);
         }
 
         cout << "#" << tc << " " << answer << "\n";
