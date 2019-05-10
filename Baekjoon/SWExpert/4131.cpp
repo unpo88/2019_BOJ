250db1e8de1ed8cc5cb584893e01b5b56aa981ed
Sub <whatsup@khu.ac.kr>
2019-05-10 22:22:46 +0900

    [Update] SWExpert 4131 활주로건설

--- Baekjoon/SWExpert/4131.cpp
+++ Baekjoon/SWExpert/4131.cpp
@@ -23,19 +23,33 @@ int main()
         init();
 
         cin >> N >> L;
-        for (int y = 0; y < N; ++y) {
-            for (int x = 0; x < N; ++x) {
+
+        for(int y = 0; y < N; ++y){
+            for(int x = 0; x < N; ++x){
                 cin >> map[y][x];
             }
         }
 
-        for (int y = 0; y < N; ++y) {
-            for (int x = 0; x < N; ++x) {
+        for(int y = 0; y < N; ++y){
+            for(int x = 0; x < N; ++x){
                 map[N + y][x] = map[x][y];
             }
         }
 
         int count = 0;
+
+        int y, x;
+
+        for(y = 0; y < 2 * N; ++y){
+            count = 1;
+            for(x = 0; x < N - 1; ++x){
+                if(map[y][x] == map[y][x + 1])  ++count;
+                else if(map[y][x] + 1 == map[y][x + 1] && count >= L)   count = 1;
+                else if(map[y][x] - 1 == map[y][x + 1] && count >= 0)   count = (1 - L);
+                else break;
+            }
+            if(x == (N - 1) && count >= 0)  ++answer;
+        }
         int i, j;
 
         for (i = 0; i < 2 * N; ++i) {
