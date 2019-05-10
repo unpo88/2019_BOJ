d8dc937310261af3d8393d3802fff331e2336d15
Sub <whatsup@khu.ac.kr>
2019-05-10 20:56:27 +0900

    [Update] SWExpert 5653 줄기세포배양

--- Baekjoon/SWExpert/5653.cpp
+++ Baekjoon/SWExpert/5653.cpp
@@ -3,24 +3,28 @@
 #include <queue>
 using namespace std;
 
+#define MAX 350
+#define BASE 150
+
 struct CELL{
-    int power;
+    int life;
     int time;
 };
+
+int answer;
+
+int N, M, K;
+
+CELL map[MAX][MAX];
 
 const int dy[] = { 0, 0, 1, -1 };
 const int dx[] = { 1, -1, 0, 0 };
 
-int answer;
- 
-int N, M, K;
-
-CELL map[600][600];
-
 void init(){
-    for(int y = 0; y < 600; ++y){
-        for(int x = 0; x < 600; ++x){
-            map[y][x].power = 0;
+    answer = 0;
+    for(int y = 0; y < MAX; ++y){
+        for(int x = 0; x < MAX; ++x){
+            map[y][x].life = 0;
             map[y][x].time = 0;
         }
     }
@@ -35,7 +39,6 @@ int main(){
 
     for(int tc = 1; tc <= T; ++tc){
         init();
-        answer = 0;
 
         cin >> N >> M >> K;
 
@@ -45,48 +48,42 @@ int main(){
             for(int x = 0; x < M; ++x){
                 int num;
                 cin >> num;
-
-                map[300 + y][300 + x].power = num;
-                map[300 + y][300 + x].time = 2 * num;
-
-                if(num != 0){
-                    q[num].push(make_pair(300 + y, 300 + x));
-                }
+                map[y + BASE][x + BASE].life = num;
+                map[y + BASE][x + BASE].time = num * 2;
+                if(num) q[num].push(make_pair(y + BASE, x + BASE));
             }
-        }
+        }        
 
         while(K--){
             for(int index = 10; index >= 1; --index){
                 int qSize = q[index].size();
-
                 while(qSize--){
-                    int cur_y = q[index].front().first;
-                    int cur_x = q[index].front().second;
+                    int y = q[index].front().first;
+                    int x = q[index].front().second;
                     q[index].pop();
 
-                    --map[cur_y][cur_x].time;
+                    --map[y][x].time;
 
-                    if(map[cur_y][cur_x].power > map[cur_y][cur_x].time){
+                    if(map[y][x].life > map[y][x].time){
                         for(int dir = 0; dir < 4; ++dir){
-                            int ny = cur_y + dy[dir];
-                            int nx = cur_x + dx[dir];
+                            int ny = y + dy[dir];
+                            int nx = x + dx[dir];
 
-                            if(map[ny][nx].power == 0){
-                                map[ny][nx].power = map[cur_y][cur_x].power;
-                                map[ny][nx].time = 2 * map[cur_y][cur_x].power;
-
+                            if(map[ny][nx].life == 0){
+                                map[ny][nx].life = map[y][x].life;
+                                map[ny][nx].time = 2 * map[y][x].life;
                                 q[index].push(make_pair(ny, nx));
                             }
                         }
                     }
 
-                    if(map[cur_y][cur_x].time != 0){
-                        q[index].push(make_pair(cur_y, cur_x));
+                    if(map[y][x].time != 0){
+                        q[index].push(make_pair(y, x));
                     }
                 }
             }
         }
-        
+
         for(int index = 1; index <= 10; ++index){
             answer += q[index].size();
         }
