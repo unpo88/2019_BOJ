38e31068ac258b34e8635ae0b856a88ca91152d6
Sub <whatsup@khu.ac.kr>
2019-05-10 22:39:41 +0900

    [Update] Final SWExpert 4131 활주로건설

--- Baekjoon/SWExpert/4131.cpp
+++ Baekjoon/SWExpert/4131.cpp
@@ -50,28 +50,6 @@ int main()
             }
             if(x == (N - 1) && count >= 0)  ++answer;
         }
-        int i, j;
-
-        for (i = 0; i < 2 * N; ++i) {
-            count = 1;
-            for (j = 0; j < N - 1; ++j) {
-                if (map[i][j] == map[i][j + 1]) {
-                    ++count;
-                }
-                else if (map[i][j] + 1 == map[i][j + 1] && count >= L) {
-                    count = 1;
-                }
-                else if (map[i][j] - 1 == map[i][j + 1] && count >= 0) {
-                    count = (1 - L);
-                }
-                else {
-                    break;
-                }
-            }
-            if (j == (N - 1) && count >= 0) {
-                ++answer;
-            }
-        }
         cout << "#" << tc << " " << answer << "\n";
     }
 	return 0;
